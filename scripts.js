9b39a22b5a1175476a6b13bdd23134060570b5d0
RitwickDutta5 <129434817+RitwickDutta5@users.noreply.github.com>
2023-05-22 17:13:21 -0400

    getting the map to load
    first attempt

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// use mapbox mercator projection along with access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    bounds: [64.81236, 3.14440, 98.33055, 38.38267],
+    zoom: 2,
+});
+// add navigation control
+map.addControl(new mapboxgl.NavigationControl());
+
+map.on('load', function () {
+    map.addSource('states-india', {
+        type: 'geojson',
+        data: 'data/states-india.geojson',
+    }),
+
+        map.addLayer({
+            id: 'fill-states-india',
+            type: 'fill',
+            source: 'states-india',
+        })
+    map.addLayer({
+        id: 'line-states-india',
+        type: 'line',
+        source: 'states-india',
+        paint: {
+            'line-color': '#FFFFFF'
+        }
+    })
+
+})
+
+
